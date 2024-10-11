3074b83824602c86edbf6f7792717c1df302d739
Arnima <arnima.jain@360degreecloud.in>
2024-10-11 16:05:50 +0530

    created a express server

---
+++ src/app.js
@@ -0,0 +1,17 @@
+//starting point of appplication
+//core application code
+
+//imports
+const express = require('express');
+
+
+//main server
+const app = express();
+//request handler function as arrow function
+app.use("/test",(req,res)=>{
+    res.send("hello from server");
+})
+
+app.listen(3000,()=>{
+    console.log('server is running on port 3000');
+});
