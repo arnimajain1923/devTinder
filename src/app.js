1c9ef1873f304ac624496f34e13c2e45616a0a4c
Arnima <arnimaj923@gmail.com>
2024-10-15 14:09:41 +0530

    playing with route handlers

--- src/app.js
+++ src/app.js
@@ -8,8 +8,16 @@ const express = require('express');
 //main server
 const app = express();
 //request handler function as arrow function
-app.get("/test",(req,res)=>{
-    res.send("hello from server");
+app.get("/test",
+    (req,res,next)=>{
+   
+    //route handler function
+    next();
+    res.send("hello from server with get api request from first route handler");
+    },
+(req,res)=>{
+
+    res.send("hello from server with get api request from second route handler");
 });
 
 app.listen(3000,()=>{
