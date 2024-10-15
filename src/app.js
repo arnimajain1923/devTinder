0645f1c151b38219260a3c7ee066c8667287de82
Arnima <arnimaj923@gmail.com>
2024-10-15 13:10:00 +0530

    created a server

--- src/app.js
+++ src/app.js
@@ -8,9 +8,9 @@ const express = require('express');
 //main server
 const app = express();
 //request handler function as arrow function
-app.use("/test",(req,res)=>{
+app.get("/test",(req,res)=>{
     res.send("hello from server");
-})
+});
 
 app.listen(3000,()=>{
     console.log('server is running on port 3000');
