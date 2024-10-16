c8c20c54c414d90efe9ca079281efa077ee4f91f
Arnima <arnimaj923@gmail.com>
2024-10-16 16:48:20 +0530

    basic user and feed apis

--- src/app.js
+++ src/app.js
@@ -16,9 +16,124 @@ app.post("/signup",async(req,res)=>{
     await user.save();
     res.send("user added succesfully");
  }catch(err){
-    res.status(401).send("error saving the user!!!",err);
+    res.status(400).send("error saving the user!!!",err);
  }
 });
+
+//feed API - get/feed - get all users from the databse
+app.get("/feed",async(req,res)=>{
+   
+    try{
+        const users = await User.find({});
+        if(users.length===0){
+          res.status(404).send("user not found");  
+        }
+        else{
+            res.send(users);
+        }
+        
+    }catch(err){
+        console.log(err);
+        res.status(400).send("something went wrong");
+    }
+});
+
+//get user by id
+app.get("/id",async(req,res)=>{
+    const id = req.body._id;
+  try{
+        const user = await User.findById(id);
+        if(!user){
+            res.status(404).send("user not found")
+        }
+        else{
+            res.send(user);
+        }
+    }catch(err){
+        console.log(err);
+        res.status(400).send("something went wrong");
+    }
+});
+
+//get users by email id
+app.get("/user",async(req,res)=>{
+    const userEmail = req.body.emailId;
+    try{
+        
+        const users = await User.find({emailId:userEmail});
+        if(users.length===0){
+          res.status(404).send("user not found");  
+        }
+        else{
+            res.send(users);
+        }
+        
+    }catch(err){
+        console.log(err);
+        res.status(400).send("something went wrong");
+    }
+});
+
+//delete user by id
+
+app.delete("/user",async(req,res)=>{
+    const userId = req.body._id;
+    try{
+        
+        const user = await User.findByIdAndDelete(userId);
+        if(user.length===0){
+          res.status(404).send("user does not exist");  
+        }
+        else{
+            res.send("user deleted succesfully");
+        }
+        
+    }catch(err){
+        res.status(400).send("something went wrong");
+    }
+});
+
+//update user by email
+app.patch("/user/email",async(req,res)=>{
+    const data = req.body;
+    const userEmail = req.body.emailId;
+    try{
+        
+        let user= await User.updateOne({emailId:userEmail},data);
+        if(user.length===0){
+          res.status(404).send("user does not exist");  
+        }
+        else{
+            console.log(user);
+            res.send("user updated succesfully");
+        }
+        
+    }catch(err){
+        res.status(400).send("something went wrong");
+    }
+});
+
+//update user by id
+app.patch("/user",async(req,res)=>{
+    const data = req.body;
+    const userId = req.body._id;
+    try{
+        
+        const user = await User.findByIdAndUpdate(userId , data,{new:true}); 
+        if(user.length===0){
+          res.status(404).send("user does not exist");  
+        }
+        else{
+            console.log(user);
+            res.send("user updated succesfully");
+        }
+        
+    }catch(err){
+        res.status(400).send("something went wrong");
+    }
+});
+
+
 //connection to devTinder database
 connectDB()
 .then(()=>{
